eff8ca99753065a7ecd27a84f16ed419a11a9020
AlexJYO <jesusgarcia0791@gmail.com>
2021-08-13 23:09:57 -0500

    Add files via upload

--- backend/aesSaveRequestAdm.php
+++ backend/aesSaveRequestAdm.php
@@ -30,7 +30,13 @@
 		$marca= $_POST['marca'];
 		$unica_repetitiva= $_POST['unica_repetitiva'];
 		$min= $_POST['min'];
+		if (!(is_numeric($min))) {
+    		$min=0;
+		} 
 		$max= $_POST['max'];
+		if (!(is_numeric($max))) {
+    		$max=0;
+		} 
 		$consumo_mensual= $_POST['consumo_mensual'];
 		$donde_se_usa= $_POST['donde_se_usa'];
 		$fuente_informacion= $_POST['fuente_informacion'];
@@ -45,24 +51,37 @@
 		$tipo_compra= $_POST['tipo_compra'];
 		$cuenta= $_POST['cuenta'];
 		$no_sc_solped= $_POST['no_sc_solped'];
+		if (!(is_numeric($no_sc_solped))) {
+    		$no_sc_solped=0;
+		} 
 		$fecha_liberacion= $_POST['fecha_l'];
 		$costo_unitario= $_POST['costo_unitario'];
+		if (!(is_numeric($costo_unitario))) {
+    		$costo_unitario=0.0;
+		} 
 		$costo_total_1= $_POST['costo_total_1'];
+		if (!(is_numeric($costo_total_1))) {
+    		$costo_total_1=0.0;
+		} 
 		$moneda_1= $_POST['moneda_1'];
 		$tiempo_estimado= $_POST['tiempo_estimado'];
 		$aprobador_actual= $_POST['aprobador_actual'];
 		$comprador_sap= $_POST['comprador_sap'];
 		$po= $_POST['po'];
+		if (!(is_numeric($po))) {
+    		$po=0;
+		} 
 		$fecha_po= $_POST['fecha_po'];
 		$proveedor= $_POST['proveedor'];
 		$fecha_realizacion= $_POST['fecha_realizacion'];
 		$no_factura= $_POST['no_factura'];
 		$costo_total_2= $_POST['costo_total_2'];
+		if (!(is_numeric($costo_total_2))) {
+    		$costo_total_2=0.0;
+		} 
 		$moneda_2= $_POST['moneda_2'];
 		$ir= $_POST['ir'];
 		$fecha_ir= $_POST['fecha_ir'];
-		echo $fecha_liberacion;
-		
 
 		$query = $db->connect()->prepare("UPDATE request SET fecha_s='$fecha_s', status= '$status', nombre_s='$nombre_s', nombre_lgt='$nombre_lgt', departamento='$departamento', tripulacion='$tripulacion', descripcion='$descripcion', cantidad='$cantidad', u_medida='$u_medida', no_parte='$no_parte', codigo_gm='$codigo_gm', marca='$marca', unica_repetitiva='$unica_repetitiva', min='$min', max='$max', consumo_mensual='$consumo_mensual', donde_se_usa='$donde_se_usa', fuente_informacion='$fuente_informacion', porque1='$porque1', porque2='$porque2', porque3='$porque3', prioridad='$prioridad', alcance_trabajo='$alcance_trabajo', dibujo_tecnico='$dibujo_tecnico', muestra_foto='$muestra_foto', tipo_compra='$tipo_compra', cuenta='$cuenta', no_sc_solped='$no_sc_solped',  costo_unitario='$costo_unitario', costo_total_1='$costo_total_1', moneda_1='$moneda_1',  aprobador_actual='$aprobador_actual', comprador_sap='$comprador_sap', po='$po',  proveedor='$proveedor', no_factura='$no_factura', costo_total_2='$costo_total_2', moneda_2='$moneda_2', ir='$ir' WHERE id = '$id_request'");
 		$result=$query->execute();
@@ -94,6 +113,19 @@
 			$query = $db->connect()->prepare("UPDATE request SET fecha_po='$fecha_po' WHERE id = '$id_request'");
 			$result=$query->execute();
 		}
+
+		$id = $_SESSION['id'];
+		$query = $db->connect()->prepare('SELECT *FROM user WHERE id = :id');
+		$query->execute(['id' => $id]);
+		$row = $query->fetchAll();
+
+		$name_all=$row[0]['nombre']." ".$row[0]['apellido'];
+		date_default_timezone_set("America/Mexico_City");
+		$today = getdate();
+		$day = $today['year'].'-'.$today['mon'].'-'.$today['mday']." ".$today['hours'].":".$today['minutes'].":".$today['seconds'];
+
+		$query = $db->connect()->prepare("UPDATE request SET nombre_r='$name_all', fecha_r='$day' WHERE id = '$id_request'");
+			$result=$query->execute();
 	}
 
 ?>
